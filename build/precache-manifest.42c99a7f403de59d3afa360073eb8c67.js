708508724d32a88f1a71535a6d0ca554a3a76b2b
syntax-js <65830347+syntax-js@users.noreply.github.com>
2020-07-21 05:41:27 +0530

    Add files via upload
    Added frontend

---
+++ build/precache-manifest.42c99a7f403de59d3afa360073eb8c67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d98fd2908dae80a57e1ae887c4de49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac99e8eb4aebe0e61ff3",
+    "url": "/static/css/2.fc5a9450.chunk.css"
+  },
+  {
+    "revision": "5f46306ac6ed3c9edc22",
+    "url": "/static/css/main.e79f5d88.chunk.css"
+  },
+  {
+    "revision": "ac99e8eb4aebe0e61ff3",
+    "url": "/static/js/2.a0c6a428.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a0c6a428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f46306ac6ed3c9edc22",
+    "url": "/static/js/main.80527a82.chunk.js"
+  },
+  {
+    "revision": "e8ca3c8c3251f267b984",
+    "url": "/static/js/runtime-main.3ee5468e.js"
+  }
+]);
